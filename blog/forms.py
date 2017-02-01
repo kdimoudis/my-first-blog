2dd8a9a2d1f94de382353bd899d9f478757cba7e
kostis <kostis@example.com>
2017-02-01 02:49:39 +0200

    Details

--- blog/forms.py
+++ blog/forms.py
@@ -6,4 +6,4 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('category', 'composer', 'label', 'image', 'title', 'text', 'rating', 'website')
